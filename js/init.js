856adaa377aca6314cdb4c0215a5dfa0a3a6eeec
michermd <michermd@gmail.com>
2018-07-19 16:52:54 -0400

    Pair proggraming Indira/Mich

--- js/init.js
+++ js/init.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
+  var API_KEY = "b24696a93ed741db75a79ea310fe8871";
   var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+  var URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently?exclude=minutely?exclude=daily";
   var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
 
+  fetch(URL)
+    .then(function(response){
+          return response.json()
+        })
+        .then(function(myJson){
+          console.log(getHours(myJson))
+          //console.log(myJson)
+          //console.log('first item:', myJson['hourly']['data'][0]['time'])
+          //var data = generateDataSet(getHours(json), getFahrenheits(json));
+          //var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+        })
+ });
 
-  makeRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
-});
+ //   makeRequest(URL, function(json) {
+ //     var data = generateDataSet(getHours(json), getFahrenheits(json));
+ //     var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+ //   });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -4,6 +4,24 @@ function getFahrenheits(result){
 
 function getHours(result){
   // Your code goes here
+  timesAndtemps = result['hourly']['data']; //array of objects with time and temperature keys
+  let hours = [];
+  let militaryTimes = []
+  for (let el in timesAndtemps){
+    hours.push(timesAndtemps[el]['time'])
+   }
+
+   function convertTime(hours) {
+     for (let prop of hours) {
+       let timeStr = new Date(prop*1000).toString()
+       let timeArr = timeStr.split(" ")
+       let milTime = timeArr[4]
+       //console.log('milTime:', milTime)
+       militaryTimes.push(milTime)
+     }
+   }
+   convertTime(hours);
+   return militaryTimes;
 }
 
 function generateDataSet(labels, data) {
